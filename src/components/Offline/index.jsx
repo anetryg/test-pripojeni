2f80a67d389f0c432a138aa1d5c9ca399e9a9dc0
anetryg <58123887+anetryg@users.noreply.github.com>
2022-09-29 20:20:45 +0200

    feat: connection check

--- src/components/Offline/index.jsx
+++ src/components/Offline/index.jsx
@@ -1,17 +1,33 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 
 const Offline = () => {
   const [isOnline, setIsOnline] = useState(navigator.onLine);
 
-  if (isOnline) {
-    return (
-      <p>Všechno v pořádku</p>
-    )
+  const handleOffline = () => {
+    console.log("handleOffline");
+    setIsOnline(false);
   };
 
-  return (
-    <p>Jste offline!</p>
-  );
+  const handleOnline = () => {
+    setIsOnline(true);
+  };
+
+  useEffect(() => {
+    window.addEventListener("offline", handleOffline);
+    window.addEventListener("online", handleOnline);
+    console.log("mounted");
+    return () => {
+      console.log("unmount");
+      window.removeEventListener("offline", handleOffline);
+      window.removeEventListener("online", handleOnline);
+    };
+  }, []);
+
+  if (isOnline) {
+    return <p>Všechno v pořádku</p>;
+  }
+
+  return <p>Jste offline!</p>;
 };
 
 export default Offline;
